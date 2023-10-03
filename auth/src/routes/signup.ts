dedd654267c680dcc5830d8d9029fd8928280f02
abhishekchaturvedi-07 <abhishekchaturvedi789@gmail.com>
2023-10-03 22:57:54 +0530

    Database Connection from Service

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,12 @@
 import {body, validationResult} from 'express-validator'
 import express, {Request, Response} from 'express';
 
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user'; // <- To connect to the database
+
+// import { DatabaseConnectionError } from '../errors/database-connection-error'; <- No needed because no it will be handled by User model
+
+
 const bodyParser = require('body-parser')
 
 const router = express.Router();
@@ -14,7 +18,8 @@ router.post('/api/users/signup',[
       .trim()
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
-  ], (req : Request, res: Response) => {
+  ], 
+  async (req : Request, res: Response) => {
     
     // res.send('Hello There from Sign Up Router')
     // const { email, password} = req.body
@@ -40,12 +45,33 @@ router.post('/api/users/signup',[
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+
+    // No needed to test like this 
+    // console.log("Creating a user...");
+    // throw new DatabaseConnectionError();
     
 
-   
+    // Instead user User Model now
+    // *********MAIN CODE TO CONNECT TO DB STARTS HERE********
+    
+    const { email, password }  = req.body;
 
+    //check if this email already exists
+
+    const existingUser = await User.findOne({ email: email });
+
+    if(existingUser)
+    {
+      console.log("Email already exists", email);
+      return res.send(email+' <- already exists');
+    }
+   
+    // Create new user and save it to database
+
+    const user = User.build({email, password})
+    await user.save()  // <-To save user to database
+    console.log("New User created", user)
+    res.status(201).send(user)
 
 })
 
