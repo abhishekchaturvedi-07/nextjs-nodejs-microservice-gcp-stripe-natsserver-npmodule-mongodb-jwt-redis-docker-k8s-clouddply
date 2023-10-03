77b14004cc44262fd67b08ef8b74714011295912
abhishekchaturvedi-07 <abhishekchaturvedi789@gmail.com>
2023-10-03 22:37:17 +0530

    UserModel and User Document and User.build to avoid more than 1 exports

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -6,6 +6,26 @@ interface UserAttrs {
     password: string;
 }
 
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc>{
+    build(attrs: UserAttrs):UserDoc
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document<any>{
+    email: string;
+    password: string;
+    // createdAt: string;
+    // updatedAt: string;
+}
+
+
+
+
+
 const userSchema = new mongoose.Schema({
     email:{
         type: 'string',
@@ -17,7 +37,15 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User', userSchema)
+
+// STATICS -> so that we need not to export 2 consts
+userSchema.statics.build = (attrs : UserAttrs) => {
+    return new User(attrs)
+}
+
+
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
 
 // Conflict between Typescript and Mongoose
 // new User({
@@ -28,9 +56,9 @@ const User = mongoose.model('User', userSchema)
 
 //Remove conflicts by interface attributes
 
-const buildUser = ( attrs: UserAttrs) => {
-    return new User(attrs)
-}
+// const buildUser = ( attrs: UserAttrs) => {
+//     return new User(attrs)
+// }
 
 // buildUser({
 //     email: "asds",
@@ -38,4 +66,15 @@ const buildUser = ( attrs: UserAttrs) => {
 //     // asdsad:2323 <- will trow an error
 // })
 
-export {User, buildUser} 
+
+const user = User.build({
+    email: "asd@sd.sd",
+    password: "sdsad"
+})
+
+user.email
+user.password
+// user.createdAt <- throw an error
+
+
+export {User} 
